9ab37a34e6e41ff504c254bb97268fcb100893ba
nicolao00 <nicolao00@naver.com>
2024-11-26 17:29:40 +0900

    feat: 초기 프로젝트 설정

---
+++ HealthData/App/HealthDataApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import HealthKit
+
+@main
+struct HealthDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ParticipationView()
+        }
+    }
+}
---
+++ HealthDataTests/HealthDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDataTests.swift
+//  HealthDataTests
+//
+//  Created by 박재형 on 11/26/24.
+//
+
+import Testing
+@testable import HealthData
+
+struct HealthDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
