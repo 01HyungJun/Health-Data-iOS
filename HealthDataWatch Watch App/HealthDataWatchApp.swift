69be34d4e65a4ad91db1dc01ddef95b70e2e0066
nicolao00 <nicolao00@naver.com>
2024-12-03 14:50:03 +0900

    feat: Watch에서만 수집가능한 데이터 추출을 위해 프로젝트 설정 추가

---
+++ HealthDataWatch Watch App/HealthDataWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDataWatchApp.swift
+//  HealthDataWatch Watch App
+//
+//  Created by 박재형 on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthDataWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
