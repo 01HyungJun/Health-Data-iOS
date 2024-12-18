6e766436d00e74f33e3e65df5987a296cf2f38c5
nicolao00 <nicolao00@naver.com>
2024-12-18 17:20:27 +0900

    fix: Register 버튼 클릭 이후  백그라운드 작업 등록되도록 변경

--- HealthData/App/HealthDataApp.swift
+++ HealthData/App/HealthDataApp.swift
@@ -1,15 +1,9 @@
 import SwiftUI
 import HealthKit
 import WatchConnectivity
-import BackgroundTasks
 
 @main
 struct HealthDataApp: App {
-    init() {
-        // 앱 시작 시 백그라운드 태스크 등록
-        BackgroundTaskManager.shared.registerBackgroundTask()
-    }
-    
     var body: some Scene {
         WindowGroup {
             ParticipationView()
